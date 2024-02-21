f09b2d4ea739efaa1afdd9c58a78cd3699eeaa94
Benjamin <Officialbenragland@gmail.com>
2024-02-20 23:59:56 -0500

    Add page theme colors to Themes Folder and Fonts

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,35 @@ const App = () => {
   return (
     <div>
       <MenuBar />
+      <header>
+        <Header />
+      </header>
+      <section>
+        <Section />
+      </section>
+      <section>
+        <Section />
+      </section>
+      <section>
+        <Section />
+      </section>
+      <section>
+        <Section />
+      </section>
+      <section>
+        <Section />
+      </section>
+
+      <section>
+        <NumbersSection />
+      </section>
+      <section>
+        <Manifesto />
+      </section>
+
+      <footer>
+        <Footer />
+      </footer>
       <h1>Helloooooooooooo World</h1>
     </div>
   )
---
+++ src/Themes/colors.js
@@ -0,0 +1,14 @@
+const colors ={
+    '--black':'#000000',
+    '--white':'#FFFFFF',
+    '--orange':'#FF5F00',
+
+}
+
+const sectionColors =  {
+    '--page-orange':'#FAAC6C',
+    '--page-yellow':'#F8FA6C',
+    '--page-gray':'#E2E2E2',
+    '--page-blue': '#8DCCFC'
+}
+
